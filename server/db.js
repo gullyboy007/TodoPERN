ef1b69bb7e941652ca24b8d08f3f0e63fad3aaa2
gullyboy007 <abhineet.anand.2000@gmail.com>
2021-02-23 19:11:44 +0530

    updated client

---
+++ server/db.js
@@ -0,0 +1,11 @@
+const Pool = require("pg").Pool;
+
+const pool = new Pool({
+    user: "abhineet",
+    password: "abhi6123",
+    host: "localhost",
+    port: 5432,
+    database: "perntodo"
+});
+
+module.exports = pool;
